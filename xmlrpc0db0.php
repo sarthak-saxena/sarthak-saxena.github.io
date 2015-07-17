5bbc8a0d6efe5b77b56b88d8158a6774922946a9
sarthak-saxena <sarthakvit@gmail.com>
2015-07-17 21:53:55 +0530

    commit sarthak.co

---
+++ xmlrpc0db0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://themes.blahlab.com/pn</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://themes.blahlab.com/pn/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://themes.blahlab.com/pn/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://themes.blahlab.com/pn/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://themes.blahlab.com/pn/xmlrpc.php" />
+          </apis>
+  </service>
+</rsd>
