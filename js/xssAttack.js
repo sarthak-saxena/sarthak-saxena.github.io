786259b68606fcf147052e407fab4021ef933ffc
Sarthak saxena <>
2020-12-23 18:24:11 +0530

    update error message in XSS

--- js/xssAttack.js
+++ js/xssAttack.js
@@ -1,2 +1,2 @@
-alert('you have been attacked by xss scripting')
+alert('you have been attacked by XSS(Cross Site Scripting)')
 document.body.innerHTML = '<div style="height: 100%; width: 100%; background: red"><h1 style="color: white">Attacked by XSS</h1></div>'
